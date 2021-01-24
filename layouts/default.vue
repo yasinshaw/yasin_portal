62527a0b36501983e9c31dc3660e216c82295451
yasinshaw <yasinshaw@gmail.com>
2021-01-24 15:50:48 +0800

    add articles

--- layouts/default.vue
+++ layouts/default.vue
@@ -97,14 +97,14 @@ export default {
       fixed: false,
       items: [
         {
-          icon: 'mdi-apps',
-          title: 'Welcome',
+          icon: 'mdi-home',
+          title: '首页',
           to: '/'
         },
         {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
+          icon: 'mdi-book',
+          title: '文章',
+          to: '/articles'
         }
       ],
       miniVariant: false,
